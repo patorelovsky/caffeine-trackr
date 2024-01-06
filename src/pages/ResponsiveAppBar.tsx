4421d4ec958a8b435b9f93a09c91f7cc02e1117d
Patrik Relovsky <pato.relovsky@gmail.com>
2024-01-06 18:01:08 +0100

    Organize imports

--- src/pages/ResponsiveAppBar.tsx
+++ src/pages/ResponsiveAppBar.tsx
@@ -1,19 +1,19 @@
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 import InsightsIcon from "@mui/icons-material/Insights";
+import MenuIcon from "@mui/icons-material/Menu";
+import AppBar from "@mui/material/AppBar";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Toolbar from "@mui/material/Toolbar";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
 import { MouseEvent, useState } from "react";
-import { ResponsiveAppBarParams } from "../types/responsiveAppBarParams";
 import { ResponsiveAppBarMenuItem } from "../types/responsiveAppBarMenuItem";
+import { ResponsiveAppBarParams } from "../types/responsiveAppBarParams";
 
 export default function ResponsiveAppBar({
   label,
